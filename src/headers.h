39cc6ff6ceca2ccab6ce2c15eb2f577f06877d76
Sentinel9111 <92266462+Sentinel9111@users.noreply.github.com>
2026-05-16 14:59:05 +0200

    added BME280

---
+++ src/headers.h
@@ -0,0 +1,4 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+void BMEValues();
+#endif //HEADERS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,46 @@
 #include <Arduino.h>
+#include <headers.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define BME_SCL 22
+#define BME_SDA 21
+
+Adafruit_BME280 bme;
+
+unsigned long startTime;
+unsigned long currentTime;
+unsigned long delayTime = 1000;
+
 void setup() {
-// write your initialization code here
+Serial.begin(9600);
+    Serial.println(F("Waterboei"));
+
+    if (!bme.begin(0x77, &Wire)) { // could be 0x76
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
+    }
+    startTime = millis();
 }
 
 void loop() {
-// write your code here
+    currentTime = millis();
+    if (currentTime - startTime >= delayTime) {
+        BMEValues();
+        startTime = currentTime;
+    }
+}
+
+void BMEValues() {
+    Serial.print("Temperature: ");
+    Serial.print(bme.readTemperature());
+    Serial.println("°C");
+    Serial.print("Humidity: ");
+    Serial.print(bme.readHumidity());
+    Serial.println("%");
+    Serial.print("Pressure: ");
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println("hPa");
 }
